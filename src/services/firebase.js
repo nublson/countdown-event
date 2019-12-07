f02be0e857d7dce4863b1fca771b097597ae4248
the-n-letter <therealnletter@hotmail.com>
2019-12-07 20:42:33 +0000

    fix some firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,16 +10,16 @@ import "firebase/firestore"
     messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
 } */
 
-const App = firebase
-    .initializeApp({
-        apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-        authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-        projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-    })
-    .firestore()
+const App = firebase.initializeApp({
+    apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+    authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+})
+
+const firestore = App.firestore()
 
 //! Initialize Firebase
-export default App
+export default firestore
