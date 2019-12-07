3b8c91b914389ce78247ca86b0611ba0ec5fbdcf
the-n-letter <therealnletter@hotmail.com>
2019-12-07 20:29:24 +0000

    replace config var

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,25 @@
 import * as firebase from "firebase/app"
 import "firebase/firestore"
 
-const config = {
+/* const config = {
     apiKey: process.env.GATSBY_FIREBASE_API_KEY,
     authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
     projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
     storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-}
+} */
 
-const App = firebase.initializeApp(config).firestore()
+const App = firebase
+    .initializeApp({
+        apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+        authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+        projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+    })
+    .firestore()
 
 //! Initialize Firebase
 export default App
